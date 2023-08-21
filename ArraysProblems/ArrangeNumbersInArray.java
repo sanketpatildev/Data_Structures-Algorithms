9654beabd48a4feb35e2d0b4468561605e405291
sanketpatildev <142790411+sanketpatildev@users.noreply.github.com>
2023-08-21 21:37:19 +0530

    Update ArrangeNumbersInArray.java

--- ArraysProblems/ArrangeNumbersInArray.java
+++ ArraysProblems/ArrangeNumbersInArray.java
@@ -3,7 +3,6 @@ package Array;
 import java.util.Scanner;
 //Question:-
 //Arrange Numbers In Array
-//Send Feedback
 //You have been given an empty array(ARR) and its size N. The only input taken from the user will be N and you need not worry about the array.
 //Your task is to populate the array using the integer values in the range 1 to N(both inclusive) in the order - 1,3,5,.......,6,4,2.
 //Note:
